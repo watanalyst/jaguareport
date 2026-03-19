7f6666dbe73bd91337f976d005f2aa11d92d501a
Weslley Tena <weslley.tena@grupobtz.com.br>
2026-03-19 11:47:12 -0300

    Criado Câmbio Período

---
+++ app/Http/Requests/Relatorios/Exportacao/CambioPeriodoPesquisarRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests\Relatorios\Exportacao;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CambioPeriodoPesquisarRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'cod_empresa'    => ['nullable', 'string'],
+            'dat_cambio_ini' => ['nullable', 'date'],
+            'dat_cambio_fim' => ['nullable', 'date'],
+            'num_processo'   => ['nullable', 'numeric'],
+            'ano_processo'   => ['nullable', 'numeric'],
+            'embarque'       => ['nullable', 'string'],
+        ];
+    }
+}
